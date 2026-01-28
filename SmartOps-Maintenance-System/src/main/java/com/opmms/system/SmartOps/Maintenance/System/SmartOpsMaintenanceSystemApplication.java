88fb6d31e07041b5576af0ee12ccdc2d61982ea0
dharaneesh <rvi13970@gmail.com>
2026-01-28 10:25:08 +0530

    Initial project setup

---
+++ SmartOps-Maintenance-System/src/main/java/com/opmms/system/SmartOps/Maintenance/System/SmartOpsMaintenanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.opmms.system.SmartOps.Maintenance.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartOpsMaintenanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartOpsMaintenanceSystemApplication.class, args);
+	}
+
+}
---
+++ SmartOps-Maintenance-System/src/test/java/com/opmms/system/SmartOps/Maintenance/System/SmartOpsMaintenanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opmms.system.SmartOps.Maintenance.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartOpsMaintenanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
